c5cdd51aeaaff57bc437d6a5eb47c40a074dee39
0902cs231028-sys <0902cs231028@rjit.ac.in>
2025-12-25 14:23:02 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * OnyxVault - Entry Point
+ */
+header("X-Frame-Options: DENY");
+header("X-Content-Type-Options: nosniff");
+header("Content-Security-Policy: default-src 'self' https://cdn.tailwindcss.com;");
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard.php");
+} else {
+    header("Location: login.php");
+}
+exit();
